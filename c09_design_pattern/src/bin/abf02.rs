c30286cda8ba9d863592f8e0de1474f1618009cf
Wade010 <19776883@qq.com>
2025-03-19 04:33:44 +0800

    update

---
+++ c09_design_pattern/src/bin/abf02.rs
@@ -0,0 +1,6 @@
+use c09_design_pattern::creational::abstract_factory::generic::test_abstract_factory;
+
+fn main() {
+    test_abstract_factory();
+}
+
---
+++ c09_design_pattern/src/creational/mod.rs
@@ -0,0 +1,3 @@
+
+pub mod abstract_factory;
+
