5a6363cf7d1cc40ff62843f5ef30b1d8edabe0a7
Wade010 <19776883@qq.com>
2025-03-21 18:59:14 +0800

    update

---
+++ c02_type_system/src/bin/trait_exp01.rs
@@ -0,0 +1,23 @@
+
+use c02_type_system::type_class::*;
+
+fn main() {
+    define::define_trait_01();
+    define::define_trait_02();
+    define::define_trait_03();
+    define::define_trait_04();
+    define::define_trait_05();
+    define::define_trait_06();
+    define::define_trait_07();
+    define::define_trait_08();
+
+    advance::advance_trait_01();
+    advance::advance_trait_02();
+    advance::advance_trait_03();
+    advance::advance_trait_04();
+    advance::advance_trait_05();
+    advance::advance_trait_06();
+    advance::advance_trait_07();
+    advance::advance_trait_08();
+    
+}
