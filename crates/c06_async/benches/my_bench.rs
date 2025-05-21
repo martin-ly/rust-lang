2246f6e60e5a770b3ba42b76023ad50225806ee0
Wade010 <19776883@qq.com>
2025-05-21 18:43:51 +0800

    update

--- crates/c06_async/benches/my_bench.rs
+++
@@ -1,9 +0,0 @@
-// benches/my_bench.rs
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
-
-fn bench_example(c: &mut Criterion) {
-    c.bench_function("example", |b| b.iter(|| black_box(42)));
-}
-
-criterion_group!(benches, bench_example);
-criterion_main!(benches);
